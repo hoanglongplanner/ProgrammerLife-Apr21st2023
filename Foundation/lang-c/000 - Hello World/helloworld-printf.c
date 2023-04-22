c47405dd3b8e1a962f14142b8122f96c693c1260
hoanglongdummy <72915871+hoanglongdummy@users.noreply.github.com>
2023-04-22 11:07:37 -0700

    Update helloworld-printf.c

--- Foundation/lang-c/000 - Hello World/helloworld-printf.c
+++ Foundation/lang-c/000 - Hello World/helloworld-printf.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
+#define K_HELLO_WORLD "Hello World Constant Define"
+
+char str_helloworld[] = "Hello World String Char Array \n";
+
 int main(){
-    printf("Hello World");
+    printf("Hello World Normal \n");
+    printf(str_helloworld);
+    printf(K_HELLO_WORLD);
     return 0;
 }
