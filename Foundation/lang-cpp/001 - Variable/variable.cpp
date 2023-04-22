a9fab0b2ac7e2a5b8f91ff623fa3d72709bd8207
Hayden Schmidt <hoanglongdummy@gmail.com>
2023-04-22 11:38:13 +0700

    Update CPP Variable

---
+++ Foundation/lang-cpp/001 - Variable/variable.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+
+int i32_value = 30;
+char char_value = 'H';
+char str_value[] = "Hello World";
+char sz_char_value[] = {'N', '0', '2'};
+std::string str_value02 = "Hello World";
+
+int main(){
+    std::cout << i32_value << std::endl;
+    return 0;
+}
