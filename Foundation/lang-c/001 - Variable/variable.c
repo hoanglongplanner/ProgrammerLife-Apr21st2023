d20b95b07bfbf7cc29bd60b631f09db72972862e
hoanglongdummy <72915871+hoanglongdummy@users.noreply.github.com>
2023-04-22 20:13:21 -0700

    Update variable.c

--- Foundation/lang-c/001 - Variable/variable.c
+++ Foundation/lang-c/001 - Variable/variable.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int i32_value = 10;
+float f_value = 0.5f;
+double d_value = 0.5f;
+long l_value = 0.5f;
+long double ld_value = 0.5f;
+bool isCondition = false;
 char str_value[] = "Hello World";
 
 int main(){
