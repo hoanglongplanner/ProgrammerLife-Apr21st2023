73160bcc56a5c7676ca6cafd5d28f31b657fb6ce
hoanglongdummy <72915871+hoanglongdummy@users.noreply.github.com>
2023-04-22 19:57:46 -0700

    Update Rust Hello World

--- Foundation/lang-rust/P000-HelloWorld/src/main.rs
+++ Foundation/lang-rust/P000-HelloWorld/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
-    let str_value = "Hello World String Variable";
+    let str_value = "Hello World String Immutable Variable"; //immutable = no value change
+    let mut str_mutValue = "Hello World String Mutable Variable"; //mutable = allow value change
 
     println!("Hello World");
     println!("{}", str_value);
+    println!("{}", str_mutValue);
 }
